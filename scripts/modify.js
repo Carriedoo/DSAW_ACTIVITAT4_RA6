d315ccd27e45d025b9f3f826970333720c2f4508
Fedeerich <marcosgongora1000@gmail.com>
2025-10-02 19:55:46 +0200

    02/10/2025 comenzando

---
+++ scripts/modify.js
@@ -0,0 +1,12 @@
+/* 
+1- Definir una variable comptador de tasques.
+2- Controlar que el formulari estigui informat. En cas contrari, es mostra una alerta.
+3- S'ha de crear un element 'div' amb el mètode 'createElement'.
+4- Afegir al div create la classe css: tasca-parell o tasca-imparell, segons el valor del comptador. S'ha de fer servir el condicional ternari.
+5- S'ha de crear un element 'h3', amb el mètode 'createElement', el text sempre serà el mateix: 'Tasca X', on x serà el número del comptador.
+6- S'ha de crear un element 'p', amb el mètode 'createElement', mostrarà la informació introduida a l'input per l'usuari.
+7- Afegir els elements 'h3' i 'p' a l'element div a partir del mètode 'appendChild'.
+8- Afegir l'element div dins del contenidor 'tasques-contanier' a partir del mètode 'insertAdjacentElement'.
+*/
+
+let comptadorTasques;
